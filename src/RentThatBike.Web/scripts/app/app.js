90d6532b1dec9451af75cede51281c7e1baf8552
Alexandru Vasile Pop <palexandru@gmail.com>
2014-04-13 17:22:38 +0100

    Added Chapter 2 Example 9 with the bicycles index view

--- src/RentThatBike.Web/scripts/app/app.js
+++ src/RentThatBike.Web/scripts/app/app.js
@@ -1,17 +1,12 @@
 ﻿(function () {
     "use strict";
 
-    var myAppModule = angular.module(
-        'myApp',
-        [
-            'ngRoute',
-            'ui.bootstrap'
-        ]);
+    var myAppModule = angular.module('myApp', ['ngRoute', 'ui.bootstrap']);
 
     myAppModule.config([
         '$routeProvider', function ($routeProvider) {
             $routeProvider.when('/', { templateUrl: 'scripts/app/views/default.html' });
-            $routeProvider.when('/bicycles', { templateUrl: 'scripts/app/views/bicyclesIndex.html' });
+            $routeProvider.when('/bicycles', { templateUrl: 'scripts/app/views/bicyclesIndex.html', controller: 'BicyclesController' });
             $routeProvider.when('/customers', { templateUrl: 'scripts/app/views/customersIndex.html' });
             $routeProvider.when('/rentals', { templateUrl: 'scripts/app/views/rentalsIndex.html' });
         }
