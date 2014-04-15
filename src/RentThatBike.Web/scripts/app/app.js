a27e924c692bea6efca11692fb1659c7f118adeb
Alexandru Vasile Pop <palexandru@gmail.com>
2014-04-15 17:48:10 +0100

    Chapter 2 Example 13 - added rentals views

--- src/RentThatBike.Web/scripts/app/app.js
+++ src/RentThatBike.Web/scripts/app/app.js
@@ -3,6 +3,15 @@
 
     var myAppModule = angular.module('myApp', ['ngRoute', 'ui.bootstrap']);
 
+    myAppModule.config(['$provide', function ($provide) {
+        $provide.decorator('$exceptionHandler', ['$delegate', function ($delegate) {
+            return function (exception, cause) {
+                $delegate(exception, cause);
+                alert(exception.message);
+            };
+        }]);
+    }]);
+
     myAppModule.config([
         '$routeProvider', function ($routeProvider) {
             $routeProvider.when('/', { templateUrl: 'scripts/app/views/default.html' });
@@ -12,7 +21,9 @@
             $routeProvider.when('/customers', { templateUrl: 'scripts/app/views/customersIndex.html', controller: 'CustomersController' });
             $routeProvider.when('/customers/new', { templateUrl: 'Scripts/app/views/customerEditor.html', controller: 'CustomerController' });
             $routeProvider.when('/customers/:customerId/edit', { templateUrl: 'Scripts/app/views/customerEditor.html', controller: 'CustomerController' });
-            $routeProvider.when('/rentals', { templateUrl: 'scripts/app/views/rentalsIndex.html' });
+            $routeProvider.when('/rentals', { templateUrl: 'scripts/app/views/rentalsIndex.html', controller: 'RentalsController' });
+            $routeProvider.when('/rentals/new', { templateUrl: 'Scripts/app/views/rentalsEditor.html', controller: 'RentalController' });
+            $routeProvider.when('/rentals/:rentalId/edit', { templateUrl: 'Scripts/app/views/rentalsEditor.html', controller: 'RentalController' });
         }
     ]);
 
---
+++ src/RentThatBike.Web/scripts/app/controllers/rentalsController.js
@@ -0,0 +1,11 @@
+﻿(function () {
+    "use strict";
+
+    var myAppModule = angular.module('myApp');
+
+    myAppModule.controller('RentalsController', ['$scope', 'rentalsService',
+            function ($scope, rentalsService) {
+                $scope.rentals = rentalsService.getRentals();
+            }
+    ]);
+})();
