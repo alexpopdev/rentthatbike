b58531af428e4429e38c0c14c2b4d1ba37560089
Alex Pop <palexandru@gmail.com>
2014-04-29 23:15:23 +0100

    started adding ASP.NET MVC support

--- src/RentThatBike.Web/Global.asax.cs
+++ src/RentThatBike.Web/Global.asax.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
 using System.Web.Security;
 using System.Web.SessionState;
 
@@ -13,6 +15,9 @@ namespace RentThatBike.Web
         protected void Application_Start(object sender, EventArgs e)
         {
             new AppHost().Init();
+
+            //AreaRegistration.RegisterAllAreas();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
         }
     }
 }
