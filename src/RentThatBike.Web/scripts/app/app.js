d94adac25117023e01251962668318e323a08600
Alex Pop <a.pop@bham.ac.uk>
2014-05-02 15:29:28 +0100

    fixed HTML5 rewrite for ServiceStack routes

--- src/RentThatBike.Web/scripts/app/app.js
+++ src/RentThatBike.Web/scripts/app/app.js
@@ -6,8 +6,8 @@
     myAppModule.config(['$provide', function ($provide) {
         $provide.decorator('$exceptionHandler', ['$delegate', function ($delegate) {
             return function (exception, cause) {
-                $delegate(exception, cause);
                 alert(exception.message);
+                $delegate(exception, cause);
             };
         }]);
     }]);
@@ -16,14 +16,14 @@
         '$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
             $routeProvider.when('/', { templateUrl: 'scripts/app/views/default.html' });
             $routeProvider.when('/bicycles', { templateUrl: 'scripts/app/views/bicyclesIndex.html', controller: 'BicyclesController' });
-            $routeProvider.when('/bicycles/new', { templateUrl: 'Scripts/app/views/bicyclesEditor.html', controller: 'BicycleController' });
-            $routeProvider.when('/bicycles/:bicycleId/edit', { templateUrl: 'Scripts/app/views/bicyclesEditor.html', controller: 'BicycleController' });
+            $routeProvider.when('/bicycles/new', { templateUrl: 'scripts/app/views/bicyclesEditor.html', controller: 'BicycleController' });
+            $routeProvider.when('/bicycles/:bicycleId/edit', { templateUrl: 'scripts/app/views/bicyclesEditor.html', controller: 'BicycleController' });
             $routeProvider.when('/customers', { templateUrl: 'scripts/app/views/customersIndex.html', controller: 'CustomersController' });
-            $routeProvider.when('/customers/new', { templateUrl: 'Scripts/app/views/customerEditor.html', controller: 'CustomerController' });
-            $routeProvider.when('/customers/:customerId/edit', { templateUrl: 'Scripts/app/views/customerEditor.html', controller: 'CustomerController' });
+            $routeProvider.when('/customers/new', { templateUrl: 'scripts/app/views/customerEditor.html', controller: 'CustomerController' });
+            $routeProvider.when('/customers/:customerId/edit', { templateUrl: 'scripts/app/views/customerEditor.html', controller: 'CustomerController' });
             $routeProvider.when('/rentals', { templateUrl: 'scripts/app/views/rentalsIndex.html', controller: 'RentalsController' });
-            $routeProvider.when('/rentals/new', { templateUrl: 'Scripts/app/views/rentalsEditor.html', controller: 'RentalController' });
-            $routeProvider.when('/rentals/:rentalId/edit', { templateUrl: 'Scripts/app/views/rentalsEditor.html', controller: 'RentalController' });
+            $routeProvider.when('/rentals/new', { templateUrl: 'scripts/app/views/rentalsEditor.html', controller: 'RentalController' });
+            $routeProvider.when('/rentals/:rentalId/edit', { templateUrl: 'scripts/app/views/rentalsEditor.html', controller: 'RentalController' });
 
             $locationProvider.html5Mode(true);
         }
