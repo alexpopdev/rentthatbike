31e0fcc20a01dbd0005999c569c386d04ebbaf1d
Alex Pop <palexandru@gmail.com>
2014-05-03 07:33:31 +0100

    Chapter4 Example3: added bundle and minification support

---
+++ src/RentThatBike.Web/App_Start/BundleConfig.cs
@@ -0,0 +1,37 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace RentThatBike.Web
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/basejs").Include(
+                        "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/moment.js"));
+
+          
+            bundles.Add(new ScriptBundle("~/bundles/angular").Include(
+                      "~/Scripts/angular.js",
+                      "~/Scripts/angular-route.js",
+                      "~/Scripts/angular-resource.js",
+                      "~/Scripts/ui-bootstrap-{version}.js"));
+            
+            bundles.Add(new ScriptBundle("~/bundles/app").Include(
+                      "~/Scripts/app/app.js",
+                      //"~/Scripts/app/filters/*.js",
+                      //"~/Scripts/app/directives/*.js",
+                      "~/Scripts/app/services/*.js",
+                      "~/Scripts/app/controllers/*.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/bootstrap.css",
+                      "~/Content/app/app.css"));
+
+            BundleTable.EnableOptimizations = false;
+            
+        }
+    }
+}
--- src/RentThatBike.Web/Global.asax.cs
+++ src/RentThatBike.Web/Global.asax.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Optimization;
 using System.Web.Routing;
 using System.Web.Security;
 using System.Web.SessionState;
@@ -18,6 +19,7 @@ namespace RentThatBike.Web
 
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
