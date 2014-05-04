567dbdab510df73cd30bb49451318019210e18ff
Alex Pop <a.pop@bham.ac.uk>
2014-05-04 18:05:36 +0100

    Chapter4 Example4: changed to use the default base controller

--- src/RentThatBike.Web/Controllers/HomeController.cs
+++ src/RentThatBike.Web/Controllers/HomeController.cs
@@ -1,13 +1,16 @@
 ﻿using System.Linq;
 using System.Web.Mvc;
 using RentThatBike.Web.ServiceModel.Types;
+using ServiceStack.CacheAccess;
 using ServiceStack.Mvc;
+using ServiceStack.ServiceInterface;
 
 namespace RentThatBike.Web.Controllers
 {
-    public class HomeController : ServiceStackController
+    public class HomeController : Controller
     {
         public BicyleRepository BicyleRepository { get; set; }
+        
         //
         // GET: /Home/
         public ActionResult Index()
