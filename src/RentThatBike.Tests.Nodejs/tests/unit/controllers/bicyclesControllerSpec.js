6d82ec69bfd60ef4ca153b58d12325b80a51a916
Alex Pop <a.pop@bham.ac.uk>
2014-05-15 10:22:40 +0100

    Chapter5: added Node.js Tools For Visual Studio
    Chapter5: added Node.js Tools For Visual Studio and initial karma
    configuration with the first passing test

---
+++ src/RentThatBike.Tests.Nodejs/tests/unit/controllers/bicyclesControllerSpec.js
@@ -0,0 +1,11 @@
+﻿'use strict';
+
+describe("bicyclesController", function () {
+
+    beforeEach(module('myApp'));
+
+    it('should ....', inject(function ($controller) {
+        var bicyclesController = $controller('BicyclesController', { $scope: {} });
+        expect(bicyclesController).toBeDefined();
+    }));
+});
