e1521fc76deed80a09a9ffcd2a5a3622daf9a1b4
Alex Pop <a.pop@bham.ac.uk>
2014-05-18 16:45:30 +0100

    Chapter 5 Example2: added service mocking

--- src/RentThatBike.Tests.Nodejs/tests/unit/controllers/bicyclesControllerSpec.js
+++ src/RentThatBike.Tests.Nodejs/tests/unit/controllers/bicyclesControllerSpec.js
@@ -8,4 +8,11 @@ describe("bicyclesController", function () {
         var bicyclesController = $controller('BicyclesController', { $scope: {} });
         expect(bicyclesController).toBeDefined();
     }));
+
+    it('should have 0 bicycles', inject(function ($controller) {
+        var $scope = {};
+        $controller('BicyclesController', { $scope: $scope, bicyclesService: { getBicycles: function (){ return []; }} });
+        expect($scope.bicycles).toBeDefined();
+        expect($scope.bicycles.length).toEqual(0);
+    }));
 });
