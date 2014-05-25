740af019c53fd3411ada464c6038f248ab56bfea
Alex Pop <a.pop@bham.ac.uk>
2014-05-25 13:42:16 +0100

    Chapter5 Example2: reverted to using $httpBackend
    Conflicts:
    	src/RentThatBike.Tests.Nodejs/tests/unit/controllers/bicycleControllerSpec.js

---
+++ src/RentThatBike.Tests.Nodejs/tests/unit/controllers/bicycleControllerSpec.js
@@ -0,0 +1,37 @@
+﻿'use strict';
+
+describe("bicycleController", function () {
+
+    beforeEach(module('myApp'));
+
+    it('should be defined', inject(function ($controller) {
+        var bicyclesController = $controller('BicycleController', { $scope: {} });
+        expect(bicyclesController).toBeDefined();
+    }));
+
+    it('should be in create new bicycle mode for the default route', inject(function ($controller) {
+        var $scope = {};
+        $controller('BicycleController', { $scope: $scope });
+
+        expect($scope.bicycle).toBeDefined();
+        expect($scope.formTitle).toEqual("Add new bicycle");
+    }));
+
+    it('should be in edit bicycle mode for the route with bicycleId set to 1', inject(function ($httpBackend, $controller) {
+        var $scope = {};
+
+        $httpBackend.expectGET('api/bicycles/1')
+           .respond({ id: 1, name: 'testBicycle1' });
+
+        $controller('BicycleController', { $scope: $scope, $routeParams: { bicycleId: 1 } });
+
+        $httpBackend.flush();
+
+        expect($scope.bicycle).toBeDefined();
+        expect($scope.formTitle).toEqual("Update bicycle");
+        expect($scope.bicycle.id).toEqual(1);
+
+        $httpBackend.verifyNoOutstandingExpectation();
+        $httpBackend.verifyNoOutstandingRequest();
+    }));
+});
