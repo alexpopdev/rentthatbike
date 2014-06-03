15fd3648679377b20c82a83cc0df7e36ee6fa1a4
Alex Pop <a.pop@bham.ac.uk>
2014-06-03 23:37:21 +0100

    Chapter6 Example6: added templatecaching support in ASP>NET MVC

--- src/RentThatBike.Web/App_Start/BundleConfig.cs
+++ src/RentThatBike.Web/App_Start/BundleConfig.cs
@@ -12,32 +12,27 @@ namespace RentThatBike.Web
                         "~/scripts/modernizr-{version}.js",
                         "~/scripts/respond.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/basejs").Include(
+            bundles.Add(new ScriptBundle("~/bundles/common-scripts").Include(
                         "~/scripts/jquery-{version}.js",
-                        "~/scripts/moment.js"));
-
-          
-            bundles.Add(new ScriptBundle("~/bundles/angular").Include(
-                      "~/scripts/angular.js",
-                      "~/scripts/angular-route.js",
-                      "~/scripts/angular-resource.js",
-                      "~/scripts/ui-bootstrap-{version}.js",
-                      "~/scripts/spin.js",
-                      "~/scripts/angular-spinner.js"));
-            
-            bundles.Add(new ScriptBundle("~/bundles/app").Include(
-                      "~/scripts/app/app.js",
-                      //"~/scripts/app/filters/*.js",
-                      //"~/scripts/app/directives/*.js",
-                      "~/scripts/app/services/*.js",
-                      "~/scripts/app/controllers/*.js"));
+                        "~/scripts/moment.js",
+                        "~/scripts/angular.js",
+                        "~/scripts/angular-route.js",
+                        "~/scripts/angular-resource.js",
+                        "~/scripts/ui-bootstrap-{version}.js",
+                        "~/scripts/spin.js",
+                        "~/scripts/angular-spinner.js",
+                        "~/scripts/app/app.js",
+                        //"~/scripts/app/filters/*.js",
+                        //"~/scripts/app/directives/*.js",
+                        "~/scripts/app/services/*.js",
+                        "~/scripts/app/controllers/*.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/content/bootstrap.css",
                       "~/content/app/app.css"));
 
-            BundleTable.EnableOptimizations = false;
-            
+            BundleTable.EnableOptimizations = true;
+
         }
     }
 }
--- src/RentThatBike.Web/Controllers/HomeController.cs
+++ src/RentThatBike.Web/Controllers/HomeController.cs
@@ -1,9 +1,14 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Web.Mvc;
+using Microsoft.Ajax.Utilities;
 using RentThatBike.Web.Models;
 using ServiceStack.Mvc;
 using ServiceStack.ServiceInterface;
 using ServiceStack.ServiceInterface.Auth;
 using ServiceStack.Text;
+using WebGrease.Css.Extensions;
 
 namespace RentThatBike.Web.Controllers
 {
@@ -14,6 +19,24 @@ namespace RentThatBike.Web.Controllers
         // GET: /Home/
         public ActionResult Index()
         {
+            var scriptsFolderPath = Server.MapPath("~/scripts/app/views");
+            var scriptsDirectoryInfo = new DirectoryInfo(scriptsFolderPath);
+
+            FileInfo[] scriptFileInfos = scriptsDirectoryInfo.GetFiles();
+            var scriptsStringBuilder = new StringBuilder();
+            scriptFileInfos.ForEach(scriptFileInfo =>
+            {
+                scriptsStringBuilder.AppendFormat(@"<script type=""text/ng-template"" id=""scripts/app/views/{0}"">",
+                    scriptFileInfo.Name);
+                scriptsStringBuilder.AppendLine();
+                scriptsStringBuilder.Append(System.IO.File.ReadAllText(scriptFileInfo.FullName));
+                scriptsStringBuilder.AppendLine();
+                scriptsStringBuilder.Append("</script>");
+                scriptsStringBuilder.AppendLine();
+            });
+
+            ViewBag.AppPartials = scriptsStringBuilder.ToString();
+
             ViewBag.AuthUserSession = UserSession;
 
             ViewBag.ServerSideDataAsJson = new ServerSideData
