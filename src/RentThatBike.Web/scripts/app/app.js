3d460d53d4b6cb0eac1197a4b6b0362d3a6dd026
Alex Pop <a.pop@bham.ac.uk>
2014-06-11 00:18:31 +0100

    changed routing to fluent API

--- src/RentThatBike.Web/scripts/app/app.js
+++ src/RentThatBike.Web/scripts/app/app.js
@@ -10,10 +10,11 @@
 
     myAppModule.config([
         '$routeProvider', function ($routeProvider) {
-            $routeProvider.when('/', { templateUrl: 'scripts/app/views/default.html' });
-            $routeProvider.when('/bicycles', { templateUrl: 'scripts/app/views/bicyclesIndex.html' });
-            $routeProvider.when('/customers', { templateUrl: 'scripts/app/views/customersIndex.html' });
-            $routeProvider.when('/rentals', { templateUrl: 'scripts/app/views/rentalsIndex.html' });
+            $routeProvider
+                .when('/', { templateUrl: 'scripts/app/views/default.html' })
+                .when('/bicycles', { templateUrl: 'scripts/app/views/bicyclesIndex.html' })
+                .when('/customers', { templateUrl: 'scripts/app/views/customersIndex.html' })
+                .when('/rentals', { templateUrl: 'scripts/app/views/rentalsIndex.html' });
         }
     ]);
 
