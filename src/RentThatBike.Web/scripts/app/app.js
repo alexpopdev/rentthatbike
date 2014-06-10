e4aa20743609fbdc91c60a2c5de89aac7b367269
Alex Pop <a.pop@bham.ac.uk>
2014-06-11 00:55:28 +0100

    changed routing to a fluent API

--- src/RentThatBike.Web/scripts/app/app.js
+++ src/RentThatBike.Web/scripts/app/app.js
@@ -5,12 +5,13 @@
 
     myAppModule.config([
         '$routeProvider', function ($routeProvider) {
-            $routeProvider.when('/', { templateUrl: 'scripts/app/views/default.html' });
-            $routeProvider.when('/bicycles', { templateUrl: 'scripts/app/views/bicyclesIndex.html', controller: 'BicyclesController' });
-            $routeProvider.when('/bicycles/new', { templateUrl: 'Scripts/app/views/bicyclesEditor.html', controller: 'BicycleController' });
-            $routeProvider.when('/bicycles/:bicycleId/edit', { templateUrl: 'Scripts/app/views/bicyclesEditor.html', controller: 'BicycleController' });
-            $routeProvider.when('/customers', { templateUrl: 'scripts/app/views/customersIndex.html' });
-            $routeProvider.when('/rentals', { templateUrl: 'scripts/app/views/rentalsIndex.html' });
+            $routeProvider
+                .when('/', { templateUrl: 'scripts/app/views/default.html' })
+                .when('/bicycles', { templateUrl: 'scripts/app/views/bicyclesIndex.html', controller: 'BicyclesController' })
+                .when('/bicycles/new', { templateUrl: 'Scripts/app/views/bicyclesEditor.html', controller: 'BicycleController' })
+                .when('/bicycles/:bicycleId/edit', { templateUrl: 'Scripts/app/views/bicyclesEditor.html', controller: 'BicycleController' })
+                .when('/customers', { templateUrl: 'scripts/app/views/customersIndex.html' })
+                .when('/rentals', { templateUrl: 'scripts/app/views/rentalsIndex.html' });
         }
     ]);
 
