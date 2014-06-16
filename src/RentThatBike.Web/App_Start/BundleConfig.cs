0b52fd658616eb2e7fb202f12079cf97d7be8e35
Alex Pop <palexandru@gmail.com>
2014-06-16 15:34:26 +0100

    add EnabledOptimizations switch to web.config
    Conflicts:
    	src/RentThatBike.Web/App_Start/BundleConfig.cs

--- src/RentThatBike.Web/App_Start/BundleConfig.cs
+++ src/RentThatBike.Web/App_Start/BundleConfig.cs
@@ -1,4 +1,6 @@
-﻿using System.Web;
+﻿using System;
+using System.Configuration;
+using System.Web;
 using System.Web.Optimization;
 
 namespace RentThatBike.Web
@@ -31,8 +33,8 @@ namespace RentThatBike.Web
                       "~/content/bootstrap.css",
                       "~/content/app/app.css"));
 
-            BundleTable.EnableOptimizations = true;
-
+            BundleTable.EnableOptimizations = Convert.ToBoolean(ConfigurationManager.AppSettings.Get("BundleTable.EnableOptimizations"));
+            
         }
     }
 }
